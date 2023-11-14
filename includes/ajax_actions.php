d248a1f824ed951a00326174d14b642ff5440731
Sajjad Hossain <sajjadh9582@gmail.com>
2023-11-14 19:56:54 +0600

    Remove Docs And blog from search result.
    - Fix some CSS

--- includes/ajax_actions.php
+++ includes/ajax_actions.php
@@ -11,11 +11,11 @@ function bbpc_search_data_fetch() {
 	$is_ajax_search_tab = $opt['is_ajax_search_tab'] ?? '';
 	global $post;
 
-	if ( class_exists( 'EazyDocs' ) || class_exists( 'bbPress' ) ) :
+	if ( class_exists( 'EazyDocs' ) || class_exists( 'bbPress' ) ) {
 		
 		// All results query
 		$all_results = new WP_Query( array(
-			'post_type'      => ['post', 'forum', 'topic', 'docs' ],
+			'post_type'      => ['forum', 'topic' ],
 			's'              => $_POST['keyword'] ?? '',
 		) );
 		$all_results_count = $all_results->found_posts;
@@ -23,30 +23,6 @@ function bbpc_search_data_fetch() {
 			$all_nsoresult = 'data-result=';
 		} else {
 			$all_nsoresult = 'data-noresult=';
-		}
-
-		// Docs query
-		$docs_query = new WP_Query( array(
-			'post_type'      => 'docs',
-			's'              => $_POST['keyword'] ?? '',
-		) );
-		$docs_count = $docs_query->found_posts;
-		if ( $docs_count > 0 ) {
-			$docs_nsoresult = 'data-result=';
-		} else {
-			$docs_nsoresult = 'data-noresult=';
-		}
-		
-		// Docs query
-		$post_query = new WP_Query( array(
-			'post_type'      => 'post',
-			's'              => $_POST['keyword'] ?? '',
-		) );
-		$post_count = $post_query->found_posts;
-		if ( $post_count > 0 ) {
-			$post_nsoresult = 'data-result=';
-		} else {
-			$post_nsoresult = 'data-noresult=';
 		}
 
 		// Forum query
@@ -62,79 +38,16 @@ function bbpc_search_data_fetch() {
 		}
 
 		?>
-		<div class="searchbar-tabs">
-			<button type="button" class="tab-item active all-active" <?php echo esc_attr( $all_nsoresult. 'No-Results-Found' ); ?> onclick="searchAllTab()">
-				<?php esc_html_e( 'All', 'bbp-core' ); ?>
-			</button>
+		<div class="tab-item active all-active" <?php echo esc_attr( $all_nsoresult. 'No-Results-Found' ); ?>></div>	
+		<?php 
+		if ( class_exists( 'bbPress' ) ) : ?>
+			<div id="search-forum" <?php echo esc_attr( $forum_nsoresult. 'No-Results-Found' ); ?> class="tab-item" ></div>
 			<?php 
-			if ( class_exists( 'EazyDocs' ) ) : 
-				?>
-				<button type="button" id="search-docs" <?php echo esc_attr( $docs_nsoresult. 'No-Results-Found' ); ?> class="tab-item" onclick="searchDocTab()">
-					<?php esc_html_e( 'Docs', 'bbp-core' ); ?>
-				</button>
-				<?php 
-			endif; 
-			?>
-			<?php if ( class_exists( 'bbPress' ) ) : ?>
-				<button type="button" id="search-forum" <?php echo esc_attr( $forum_nsoresult. 'No-Results-Found' ); ?> class="tab-item" onclick="searchForumTab()">
-					<?php esc_html_e( 'Forum', 'bbp-core' ); ?>
-				</button>
-			<?php endif; ?>
-			<button type="button" id="search-blog" <?php echo esc_attr( $post_nsoresult. 'No-Results-Found' ); ?> class="tab-item" onclick="searchBlogTab()">
-				<?php esc_html_e( 'Blog', 'bbp-core' ); ?>
-			</button>
-		</div>
-		<?php
-	endif;	
+		endif;
+	}	
 
 	echo '<div class="bbpc-search-results-wrapper">';
-	// Blog query
-	$posts = new WP_Query( array(
-		'post_type'      => 'post',
-		's'              => $_POST['keyword'] ?? '',
-		'posts_per_page' => 5,
-	) );
-	if ( $posts->have_posts() ) :
-		echo '<div class="search-results-tab post show" id="search-blog-results">';
 
-		echo '<h2 class="bbpc-search-title">' . esc_html__( 'Blog Posts', 'bbp-core' ) . '</h2>';
-		while ( $posts->have_posts() ) : $posts->the_post();
-			?>
-			<div class="search-result-item">
-				<a href="<?php the_permalink(); ?>" class="title">
-					<?php the_title() ?>
-				</a>
-			</div>
-			<?php
-		endwhile;
-		wp_reset_postdata();
-	echo '</div>';	
-	endif;
-	
-	// Docs query
-    $posts = new WP_Query( [
-            'post_type'             => 'docs',
-            's'     				=> $_POST['keyword'] ?? '',
-			'posts_per_page' 		=> 5,
-        ]
-    );
-    if ( $posts->have_posts() ):
-		echo '<div class="search-results-tab docs show" id="search-docs-results">';
-		echo '<h2 class="bbpc-search-title">' . esc_html__( 'Documentation', 'bbp-core' ) . '</h2>';
-
-        while ( $posts->have_posts() ) : $posts->the_post();
-            ?>
-            <div class="search-result-item" onclick="document.location='<?php echo get_the_permalink(get_the_ID()); ?>'">
-                <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="title">
-                    <?php the_title() ?>
-                </a>
-            </div>
-            <?php
-        endwhile;
-        wp_reset_postdata();
-		echo '</div>';
-    endif;
-	
 	// Forums query
 	if ( class_exists( 'bbPress' ) ) :
 
@@ -184,48 +97,6 @@ function bbpc_search_data_fetch() {
 
 	echo '<h5 class="not-found-text">Not Found Result!</h5>';
 	echo '</div>';
-	die();
-}
-
-/**
- * Blog Search results
- */
-add_action( 'wp_ajax_bbpc_search_data_blog', 'bbpc_search_data_blog' );
-add_action( 'wp_ajax_nopriv_bbpc_search_data_blog', 'bbpc_search_data_blog' );
-function bbpc_search_data_blog() {
-
-	$post_type 		= $_POST['post_type'] ?? '';
-	$sec_title   	= '';
-
-	if ( $post_type == 'post' ) {
-		$post_name 	= 'blog';
-		$sec_title   = 'Blog Posts';
-	} else {
-		$post_name 	= 'docs';
-		$sec_title  = 'Documentation';
-	}
-	
-	// Blog query
-	$blog = new WP_Query( array(
-		'post_type'      => $post_type,
-		's'              => $_POST['keyword'] ?? '',
-		'posts_per_page' => 5,
-	) );
-	echo '<div class="search-results-tab '.$post_name.' show" id="search-'.$post_name.'-results">';
-	if ( $blog->have_posts() ) :
-		echo '<h2 class="bbpc-search-title">' . esc_html__( $sec_title, 'bbp-core' ) . '</h2>';
-		while ( $blog->have_posts() ) : $blog->the_post();
-			?>
-			<div class="search-result-item blog" onclick="document.location='<?php echo get_the_permalink(get_the_ID()); ?>'">					
-				<a href="<?php echo get_the_permalink(get_the_ID()); ?>">
-					<?php the_title(); ?>
-				</a>
-			</div>
-			<?php
-		endwhile;
-		wp_reset_postdata();
-	endif;
-	echo '</div>';	
 	die();
 }
 
